ba55d080122eadabc8433439ad18a9dfea60faa9
Vitalii Honchar <weaxme@gmail.com>
2025-07-26 08:12:48 +0200

    Fixed app startup

--- src/agentapi/schemas/__init__.py
+++ src/agentapi/schemas/__init__.py
@@ -1,8 +1,7 @@
-from .agent_configuration import Agent, AgentConfigurationCreate, AgentConfigurationRead
+from .agent_configuration import AgentConfigurationCreate, AgentConfigurationRead
 from .agent_execution import AgentExecutionCreate, AgentExecutionRead
 
 __name__ = [
-    "Agent",
     "AgentConfigurationCreate",
     "AgentConfigurationRead",
     "AgentExecutionCreate",
